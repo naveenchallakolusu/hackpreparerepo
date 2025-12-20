75c24a5ff2111c924609bbaf0106c7e243d13030
naga-kishore <103572183+naga-kishore@users.noreply.github.com>
2025-12-20 13:28:42 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -5,6 +5,19 @@ variable "environment" {
 variable "region" {
   type = string
 }
+
 variable "acr_sku" {
+  type    = string
+}
+
+variable "kubernetes_version" {
+  type    = string
+}
+
+variable "node_count" {
+  type    = number
+}
+
+variable "node_vm_size" {
   type    = string
 }
