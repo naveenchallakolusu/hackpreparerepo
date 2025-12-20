360a42dbe6162ac8a4557ffb613bb6f7c5c53d83
naga-kishore <103572183+naga-kishore@users.noreply.github.com>
2025-12-20 13:24:07 +0530

    Update acr.tf

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -1,17 +1,8 @@
 resource "azurerm_container_registry" "acr" {
-  name                = ""
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  sku                 = "Premium"
+  name                = "acr${var.environment}${var.region}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  sku                 = var.acr_sku
   admin_enabled       = false
-  georeplications {
-    location                = "East US"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
-  georeplications {
-    location                = "North Europe"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
 }
