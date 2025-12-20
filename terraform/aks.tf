692234613b799d1d152b06913b73fb3009f80897
naga-kishore <103572183+naga-kishore@users.noreply.github.com>
2025-12-20 13:26:45 +0530

    Create aks.tf

---
+++ terraform/aks.tf
@@ -0,0 +1,24 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-${var.environment}-${var.region}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aks-${var.environment}"
+
+  kubernetes_version  = var.kubernetes_version
+
+  default_node_pool {
+    name       = "system"
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
+    type       = "VirtualMachineScaleSets"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "standard"
+  }
+}
