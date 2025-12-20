e47d472a160c7ec6cb7ccb3caae0da2eb176880b
naga-kishore <103572183+naga-kishore@users.noreply.github.com>
2025-12-20 14:58:42 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfhclhack"
+    resource_group_name  = "hclhack"
     storage_account_name = "hclhack"
     container_name       = "backend"
     key                  = "terraform.tfstate"
